746c1d93783ba0a8ec2301bd2c52c3f71ed3110a
bonnieho <bonnieho@rice.edu>
2019-01-30 23:34:38 -0600

    adding in more of the modal functionality

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -118,8 +118,23 @@
 		var winLampShowL = document.getElementById("win-lamp_L");
 		var winLampShowR = document.getElementById("win-lamp_R"); 
 
-	// in-page messaging setup (to replace alert popups)
+	// in-page messaging setup (to replace alert popups) 
 		var modal = document.getElementById("myModal");
+
+	// Get the <span> element that closes the modal
+		var span = document.getElementsByClassName("close")[0];
+
+	// When the user clicks on <span> (x), close the modal
+		span.onclick = function() {
+		  modal.style.display = "none";
+		}
+
+	// When the user clicks anywhere outside of the modal, close it
+		window.onclick = function(event) {
+		  if (event.target == modal) {
+		    modal.style.display = "none";
+		  }
+		}
 
 		// var x = document.getElementsByClassName("example");
 		// var i;
